31dd9414d6afa93026643aba2faf5c6cced52b47
Ivan Chenoweth <ivan.chenoweth@office365.3pillarglobal.com>
2023-01-24 22:54:13 -0700

    Estructura de una app de flutter

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MiApp());
+
+class MiApp extends StatelessWidget {
+  const MiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "MiApp",
+      home: Inicio(),
+    );
+  }
+}
+
+class Inicio extends StatefulWidget {
+  const Inicio({super.key});
+
+  @override
+  State<Inicio> createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Mi App"),
+      ),
+      body: const Center(child: Text("Contenido")),
+    );
+  }
+}
