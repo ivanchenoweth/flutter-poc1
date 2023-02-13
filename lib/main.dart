6e584626f0669a2aa975c44bf8346de9a80f9a3c
Ivan Chenoweth <ivan.chenoweth@office365.3pillarglobal.com>
2023-02-12 23:39:46 -0700

    adding user password and submit button

--- lib/main.dart
+++ lib/main.dart
@@ -26,12 +26,62 @@ class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Tema del video"),
-      ),
       body: Center(
-        child: const Text("Hola"),
+        child: cuerpo(),
       ),
     );
   }
+}
+
+Widget cuerpo() {
+  return Container(
+    decoration: BoxDecoration(
+        image: DecorationImage(
+      image: NetworkImage(
+          "https://milesdefondos.com/wp-content/uploads/2021/06/Fondos-de-pantalla-para-celular-de-Noche-5.jpg"),
+    )),
+    child: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      nombre(),
+      campoUsuario(),
+      campoContrasena(),
+      botonEntrar()
+    ])),
+  );
+}
+
+Widget campoUsuario() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+    child: TextField(
+        decoration: InputDecoration(
+            hintText: "User", fillColor: Colors.white, filled: true)),
+  );
+}
+
+Widget campoContrasena() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+    child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+            hintText: "Password", fillColor: Colors.white, filled: true)),
+  );
+}
+
+Widget nombre() {
+  return Text("Sign in", style: TextStyle(color: Colors.white, fontSize: 35.0));
+}
+
+Widget botonEntrar() {
+  return TextButton(
+    onPressed: () {},
+    style: TextButton.styleFrom(
+      primary: Colors.purpleAccent, // Text Color
+    ),
+    child: const Text(
+      'Text Button ',
+      style: TextStyle(fontSize: 24),
+    ),
+  );
 }
