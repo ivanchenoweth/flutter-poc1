17021d881cc70fb306af6225e384a544d9772798
Ivan R. Chenoweth <ivanchenoweth@gmail.com>
2023-02-19 16:23:47 -0700

    07 alerdialog subcribirse o no subscribirse

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_final_fields
 
 import 'package:flutter/material.dart';
 
@@ -23,15 +23,74 @@ class Inicio extends StatefulWidget {
 }
 
 class _InicioState extends State<Inicio> {
+  bool _subscrito = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Tema del video"),
+        title: const Text("07 AlertDialog"),
       ),
       body: Center(
-        child: const Text("Hola"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                padding: EdgeInsets.all(25),
+              ),
+              onPressed: () {
+                _mostrarAlerta(context);
+              },
+              child: Text("Subcribirme a mi canal V2",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Color(0xffffff00),
+                    backgroundColor: Colors.orange,
+                  )),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Text(
+              _subscrito ? "Subscrito" : "No Subscrito",
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  void _mostrarAlerta(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(_subscrito ? "Cancelar Subscripcion" : 'Subscribirse'),
+          content: Text(_subscrito
+              ? '¿Estas seguro que quieres cancelar la subcripcion?'
+              : "¿Estas seguro que quieres subscribirte?"),
+          actions: <Widget>[
+            ElevatedButton(
+              child: Text('Si quiero'),
+              onPressed: () {
+                setState(() {
+                  _subscrito = !_subscrito;
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+            ElevatedButton(
+              child: Text('Cancelar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
